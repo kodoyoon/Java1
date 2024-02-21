ee4202c9613410899136a831cc7f5497066f8266
doyoon <doyoun0527@naver.com>
2024-02-21 22:04:24 +0900

    변수 시작

---
+++ src/variable/Var2.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class Var2 {
+    public static void main(String[] args) {
+        int a;  // 변수 선언(숫자를 담는 통을 만들어줌)
+        a = 10; // 변수 초기화(그릇에 10을 넣어주는)
+
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+
+    }
+}
