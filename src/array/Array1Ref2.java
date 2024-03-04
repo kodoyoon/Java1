01c45a9f70bab5a6554b807813a660f153250c3d
doyoon <doyoun0527@naver.com>
2024-03-04 16:29:49 +0900

    배열 리펙토링

--- src/array/Array1Ref2.java
+++ src/array/Array1Ref2.java
@@ -4,7 +4,7 @@ public class Array1Ref2 {
     public static void main(String[] args) {
 
         int[] students; //배열 변수 선언
-        students = new int[5]; //배열 생성
+        students = new int[5];
 
         //변수 값 대입
         students[0] = 90;
--- src/array/Array1Ref3.java
+++ src/array/Array1Ref3.java
@@ -4,7 +4,7 @@ public class Array1Ref3 {
 
     public static void main(String[] args) {
 
-        int [] students;
+        int [] students; // 배열 변수 선언
         students = new int[]{90, 80, 70, 60, 50}; // 배열 생성과 초기화
         //int[] students = new int[]{90,80,70,60,50};
 
